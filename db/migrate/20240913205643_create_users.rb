5246dc83f3b93f515d84a256853ee7c3b92fe689
Sean Silvius <sean@silvius.me>
2024-09-13 14:58:19 -0700

    Adds user object to support authentication.

---
+++ db/migrate/20240913205643_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.2]
+  def change
+    create_table :users, id: :uuid do |t|
+      t.string :email
+      t.string :password_digest
+      t.boolean :email_validated,     default: false 
+      t.datetime :email_validated_at
+      t.boolean :cookies_accepted,    default: false
+      t.datetime :cookies_accepted_at
+
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+  end
+end
+
